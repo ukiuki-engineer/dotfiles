cb7f123039a712fcb885141f97b54f967aca28fd
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-03-04 19:56:38 +0900

    update

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 #
+# cobalt2インストール
+#
+function install_cobalt2() {
+  # cobalt2がある場合は処理終了
+  if [[ -e ~/.oh-my-zsh/themes/cobalt2.zsh-theme ]] || [[ -e ~/Cobalt2-iterm ]]; then
+    return
+  fi
+  git clone https://github.com/wesbos/Cobalt2-iterm.git
+  mv Cobalt2-iterm ~/
+  cp ~/Cobalt2-iterm/cobalt2.zsh-theme ~/.oh-my-zsh/themes/
+}
+
+#
 # シンボリックリンクを作成する関数
 # 引数:ファイル名
 #
-function makeLink() {
+function make_link() {
   # ファイルがある&&シンボリックリンク→unlink
   if [[ -e ~/$1 && -L ~/$1 ]]; then
     unlink ~/$1
   # ファイルがある&&シンボリックリンクではない→バックアップを取る
   elif [[ -e ~/$1 && ! -L ~/$1 ]]; then
     mv ~/$1 ~/$1.bak
+    echo " \"~/${1}\" のバックアップ \"~/${1}.bak\" を作成しました。"
   fi
   ln -s ${PWD}/$1 ~/$1
-  echo "${PWD}/${1}→~/${1}のシンボリックリンクを作成しました。"
-}
-
-#
-# .zshrc追記処理
-#
-function writeToZshrc() {
-  echo "# 個人設定" >> ~/.zshrc
-  echo "# 大体の設定はmy.zshに書く。.zshrcにはあまり書かないようにする" >> ~/.zshrc
-  echo "source ~/my.zsh" >> ~/.zshrc
-  echo "# source ~/dotfiles/my.zsh" >> ~/.zshrc
-  echo "# 環境ごとの設定" >> ~/.zshrc
-  echo "# 環境に依存する設定は~/.local.zshに書く" >> ~/.zshrc
-  echo "if [ -e ~/local.zsh ]; then" >> ~/.zshrc
-  echo "  source ~/local.zsh" >> ~/.zshrc
-  echo "fi" >> ~/.zshrc
+  echo "シンボリックリンク \"${PWD}/${1}→~/${1}\" を作成しました。"
 }
 
 if [[ ! -e ~/.oh-my-zsh ]]; then
   # oh-my-zshインストール
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-  # .zshrc追記
-  writeToZshrc
-  # my.zsh
-  makeLink my.zsh
-else
-  echo "FIXME: 既にoh-my-zshがインストールされている場合の処理未実装"
 fi
+
+# cobalt2のインストール
+install_cobalt2
+# .zshrc
+make_link .zshrc
+# my.zsh
+make_link my.zsh
+
