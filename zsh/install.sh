9bfaaa0d1153d73021010cd9677d94eb8650c352
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-03-04 17:55:37 +0900

    update: oh-my-zshのインストール方法変更

--- zsh/install.sh
+++ zsh/install.sh
@@ -20,9 +20,25 @@ function makeLink() {
   echo "${PWD}/${1}→~/${1}のシンボリックリンクを作成しました。"
 }
 
-# .zshrc
-makeLink .zshrc
-# my.zsh
-makeLink my.zsh
-# .oh-my-zsh
-makeLink .oh-my-zsh
+function writeToZshrc() {
+  echo "# 個人設定" >> ~/.zshrc
+  echo "# 大体の設定はmy.zshに書く。.zshrcにはあまり書かないようにする" >> ~/.zshrc
+  echo "source ~/my.zsh" >> ~/.zshrc
+  echo "# source ~/dotfiles/my.zsh" >> ~/.zshrc
+  echo "# 環境ごとの設定" >> ~/.zshrc
+  echo "# 環境に依存する設定は~/.local.zshに書く" >> ~/.zshrc
+  echo "if [ -e ~/local.zsh ]; then" >> ~/.zshrc
+  echo "  source ~/local.zsh" >> ~/.zshrc
+  echo "fi" >> ~/.zshrc
+}
+
+if [[ ! -e ~/.oh-my-zsh ]]; then
+  # oh-my-zshインストール
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  # .zshrc追記
+  writeToZshrc
+  # my.zsh
+  makeLink my.zsh
+else
+  echo "FIXME: 既にoh-my-zshがインストールされている場合の処理未実装"
+fi
--- zsh/readme.md
+++ zsh/readme.md
@@ -1,14 +1,13 @@
-## 概要
-zshの自分用設定ファイル。  
+# zshの自分用設定ファイル
+
 
 ## ディレクトリ構成
 ```
-dotfiles/
-        ├── install.sh  # インストーラ
-        ├── .zshrc      # zsh設定のメインファイル
-        ├── .my.zsh     # 追加設定
-        ├── readme.md
-        └── .oh-my-zsh/ # oh-my-zsh本体
+zsh/
+   ├── install.sh  # インストーラ
+   ├── .zshrc      # zsh設定のメインファイル
+   ├── .my.zsh     # 追加設定
+   └── readme.md
 ```
 
 ## theme
@@ -18,11 +17,6 @@ cobalt2をベースとして、以下のようにカスタマイズして使用�
 - アイコンをOSに応じて変更
 - カレントディレクトリをフルパスに
 - 最後に改行を入れる
-ソースは[.my.zshのこの行以降](https://github.com/ukiuki-engineer/dotfiles/blob/master/.my.zsh#L22)を参照。
 
 ## Fonts
 [powerlinefonts](https://github.com/powerline/fonts)
-
-## FIXME
-- oh-my-zsh導入方法の変更
-oh-my-zsh本体を直接突っ込むのではなく、oh-my-zshをインストールする処理をinstall.shに追加する
