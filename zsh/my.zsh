20fc76902a3b856e9a6362346c52999d4f0f032b
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2023-03-24 18:59:46 +0900

    update

--- zsh/my.zsh
+++ zsh/my.zsh
@@ -7,8 +7,7 @@ alias la='ls -a'
 alias ll='ls -l'
 alias grep='grep --color=auto'
 # 簡易treeコマンド。
-which tree > /dev/null 2>&1
-if [ $? != 0 ]; then
+if which tree >/dev/null 2>&1; then
   alias tree="pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'"
 fi
 
