c431ba8a5d9b8c1650dba660a9c3154741908923
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-01-10 02:25:11 +0900

    update: インストーラを修正

--- .zsh/install.sh
+++ .zsh/install.sh
@@ -7,16 +7,18 @@
 # 引数:ファイル名
 #
 function make_link() {
+  target_path=$1
+  target_name=$(basename $target_path)
   # ファイルがある&&シンボリックリンク→unlink
-  if [[ -e ~/$1 && -L ~/$1 ]]; then
-    unlink ~/$1
+  if [[ -e ~/${target_name} && -L ~/${target_name} ]]; then
+    unlink ~/${target_name}
   # ファイルがある&&シンボリックリンクではない→バックアップを取る
-  elif [[ -e ~/$1 && ! -L ~/$1 ]]; then
-    mv ~/$1 ~/$1.bak
-    echo " \"~/${1}\" のバックアップ \"~/${1}.bak\" を作成しました。"
+  elif [[ -e ~/${target_name} && ! -L ~/${target_name} ]]; then
+    mv ~/${target_name} ~/${target_name}.bak
+    echo " \"~/${target_name}\" のバックアップ \"~/${target_name}.bak\" を作成しました。"
   fi
-  ln -s ${PWD}/$1 ~/$1
-  echo "シンボリックリンク \"${PWD}/${1}→~/${1}\" を作成しました。"
+  ln -s ${target_path} ~/${target_name}
+  echo "シンボリックリンク \"${target_path}→~/${target_name}\" を作成しました。"
 }
 
 # oh-my-zshのインストール
@@ -25,17 +27,21 @@ if [[ ! -e ~/.oh-my-zsh ]]; then
 fi
 
 # cobalt2のインストール
-if [[ -e ~/.oh-my-zsh/themes/cobalt2.zsh-theme ]]; then
-  # cobalt2がある場合は処理終了
-  return
+if [[ ! -e ~/.oh-my-zsh/themes/cobalt2.zsh-theme ]]; then
+  if [[ ! -e ~/Cobalt2-iterm ]]; then
+    git clone https://github.com/wesbos/Cobalt2-iterm.git
+    mv Cobalt2-iterm ~/
+  fi
+  cp ~/Cobalt2-iterm/cobalt2.zsh-theme ~/.oh-my-zsh/themes/
 fi
-if [[ ! -e ~/Cobalt2-iterm ]]; then
-  git clone https://github.com/wesbos/Cobalt2-iterm.git
-  mv Cobalt2-iterm ~/
-fi
-cp ~/Cobalt2-iterm/cobalt2.zsh-theme ~/.oh-my-zsh/themes/
+
+here_dir=$(
+  cd $(dirname $0)
+  pwd
+)
+zshrc_path=$(realpath "${here_dir}/../.zshrc")
 
 # .zshrcのシンボリックリンクをホームディレクトリに
-make_link .zshrc
-# my.zshのシンボリックリンクをホームディレクトリに
-make_link .my.zsh
+make_link ${zshrc_path}
+# .zsh/のシンボリックリンクをホームディレクトリに
+make_link ${here_dir}
