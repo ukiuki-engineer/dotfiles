ab358d0beb95d32b4a6afe8e034f551ead430c78
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-02-13 12:34:59 +0900

    fix: バグってたので一旦元に戻す

--- .zsh/my.zsh
+++ .zsh/my.zsh
@@ -40,11 +40,7 @@ fzf_docker_exec_shell() {
   fi
 
   # トライするシェルのリスト
-  local shells="
-    bash
-    ash
-    sh
-  "
+  local shells="bash, ash, sh"
   # コンテナ名
   local container_name
   # 実行するシェル
@@ -64,7 +60,7 @@ fzf_docker_exec_shell() {
   container_name=$(docker ps --format '{{.Names}}' --filter "id=$cid")
 
   # 実行可能なshellを調べる
-  for shell in $shells; do
+  for shell in $(echo $shells | tr ', ' '\n'); do
     if docker exec $cid $shell > /dev/null 2>&1; then
       exec_shell=$shell
       break
