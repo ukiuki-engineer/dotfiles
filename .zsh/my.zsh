eea29f8452cf477e83b8867df43d5b21652c217e
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-02-26 18:32:34 +0900

    add: aliasを追加

--- .zsh/my.zsh
+++ .zsh/my.zsh
@@ -112,6 +112,7 @@ alias grep='grep --color=auto'
 alias dockerExecShell='fzf_docker_exec_shell'
 alias emoji='fzf_emoji'
 alias cds='dirs -v; echo -n "select number: "; read newdir; cd +"$newdir"'
+alias gitLogOneline='git log --oneline --graph --date=format:"%Y/%m/%d %H:%M:%S" --color=always --pretty="%C(auto)%h %C(blue)%ad %C(green)[%an]%C(reset) %s" '
 
 # ------------------------------------------------------------------------------
 # WSL固有の設定
