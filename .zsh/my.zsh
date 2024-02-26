090546d2924239b071a0283d7f53de9640e604b7
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-02-26 18:32:50 +0900

    update: 足りてなかった設定を追加

--- .zsh/my.zsh
+++ .zsh/my.zsh
@@ -5,6 +5,22 @@
 # ------------------------------------------------------------------------------
 # 基本的な設定
 # ------------------------------------------------------------------------------
+# history
+HISTFILE=$HOME/.zsh_history     # 履歴を保存するファイル
+HISTSIZE=100000                 # メモリ上に保存する履歴のサイズ
+SAVEHIST=1000000                # 上述のファイルに保存する履歴のサイズ
+
+# share .zshhistory
+setopt inc_append_history       # 実行時に履歴をファイルにに追加していく
+setopt share_history            # 履歴を他のシェルとリアルタイム共有する
+
+setopt hist_ignore_all_dups     # ヒストリーに重複を表示しない
+setopt hist_save_no_dups        # 重複するコマンドが保存されるとき、古い方を削除する。
+setopt extended_history         # コマンドのタイムスタンプをHISTFILEに記録する
+setopt hist_expire_dups_first   # HISTFILEのサイズがHISTSIZEを超える場合は、最初に重複を削除する
+
+# 補完で大文字小文字を区別しない
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # 補完候補をカーソルで選択できるように
 autoload -U compinit
 compinit
