31536dfbbfa3addd7ad3c95aa1c4e9d88a4db849
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-03-14 13:32:49 +0900

    update: manはNeovimで読む

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -5,13 +5,17 @@
 #       動作が極端に重くなったりする。
 # ------------------------------------------------------------------------------
 export LANG=ja_JP.UTF-8
+
 # manをvimで開く
 # NOTE: -Rとset noma片方ずつだと以下の問題があるため両方設定した方が良い
 # -Rのみ      : 保存はできないが編集自体はできてしまう
 # set nomaのみ: 開いた時点で編集した状態と判定されており、:qで終了できない
-# TODO: 本当はneovimにしたいけど、何故かタグジャンプが上手くいかない
-export MANPAGER="col -b -x | /usr/local/bin/vim -R -c 'set ft=man noma nu' -"
+export MANPAGER="col -b -x | nvim -R -c 'set ft=man noma nu' -"
+
+# fzf
 export FZF_DEFAULT_OPTS="--height=60% --border"
+
+# lsコマンドのオプション
 LSOPTIONS=""
 if [[ "$OSTYPE" == "darwin"* ]]; then
   LSOPTIONS=" -FG"
