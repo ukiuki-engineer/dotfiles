ed280dfaa9b392a6b5f2fbed4f0a2f3c63d3fac4
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-03-26 14:39:24 +0900

    clean: 整理

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -26,20 +26,20 @@ export LSOPTIONS
 # ------------------------------------------------------------------------------
 # 基本的な設定
 # ------------------------------------------------------------------------------
+#
 # history
+#
 HISTFILE=$HOME/.zsh_history     # 履歴を保存するファイル
 HISTSIZE=100000                 # メモリ上に保存する履歴のサイズ
 SAVEHIST=1000000                # 上述のファイルに保存する履歴のサイズ
-
-# share .zshhistory
 setopt inc_append_history       # 実行時に履歴をファイルにに追加していく
 setopt share_history            # 履歴を他のシェルとリアルタイム共有する
-
 setopt hist_ignore_all_dups     # ヒストリーに重複を表示しない
 setopt hist_save_no_dups        # 重複するコマンドが保存されるとき、古い方を削除する。
 setopt extended_history         # コマンドのタイムスタンプをHISTFILEに記録する
 setopt hist_expire_dups_first   # HISTFILEのサイズがHISTSIZEを超える場合は、最初に重複を削除する
 
+#
 # 補完の設定
 #
 # 補完候補をカーソルで選択
@@ -50,7 +50,9 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|=* l:|=*'
 autoload -U compinit
 compinit
 
+#
 # cd周り
+#
 # auto_ls
 function chpwd(){
   if [[ $(pwd) != $HOME ]]; then;
@@ -58,18 +60,20 @@ function chpwd(){
   fi
 }
 autoload chpwd
-
-# viモード
-bindkey -v
-# <C-f>でautosuggestを受け入れる
-bindkey -M viins '^f' autosuggest-accept
-
 # ディレクトリ名だけでcdする
 setopt AUTO_CD
 # cd したら自動的にpushdする
 setopt AUTO_PUSHD
 # 重複したディレクトリをスタックに追加しない
 setopt PUSHD_IGNORE_DUPS
+
+#
+# キーバインド
+#
+# viモード
+bindkey -v
+# <C-f>でautosuggestを受け入れる
+bindkey -M viins '^f' autosuggest-accept
 # ------------------------------------------------------------------------------
 # WSL固有の設定
 # ------------------------------------------------------------------------------
